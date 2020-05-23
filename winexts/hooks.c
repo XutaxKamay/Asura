b12ac0713f1bc23c4be847571def0ff644b7d2a0
kamay <xutaxkamay@gmail.com>
2020-05-24 00:08:29 +0200

    Renamed variable
    call to jmp

--- winexts/hooks.c
+++ winexts/hooks.c
@@ -22,24 +22,24 @@ bool hook_rel_call(uintptr_t call_inst_location, uintptr_t new_func)
     return true;
 }
 
-bool hook_rel_jmp(uintptr_t call_inst_location, uintptr_t new_func)
+bool hook_rel_jmp(uintptr_t jmp_inst_location, uintptr_t new_func)
 {
     pteval_t old_pte_val;
 
-    old_pte_val = get_page_flags(call_inst_location + 1);
-    set_page_flags(call_inst_location + 1, old_pte_val | _PAGE_RW);
+    old_pte_val = get_page_flags(jmp_inst_location + 1);
+    set_page_flags(jmp_inst_location + 1, old_pte_val | _PAGE_RW);
 
-    if (*(unsigned char*)call_inst_location != 0xE9)
+    if (*(unsigned char*)jmp_inst_location != 0xE9)
     {
-        set_page_flags(call_inst_location + 1, old_pte_val);
+        set_page_flags(jmp_inst_location + 1, old_pte_val);
         return false;
     }
 
-    *(uint32_t*)(call_inst_location + 1)
+    *(uint32_t*)(jmp_inst_location + 1)
       = ((uint32_t)(new_func & 0xFFFFFFFF))
-        - (((uint32_t)call_inst_location + 5) & 0xFFFFFFFF);
+        - (((uint32_t)jmp_inst_location + 5) & 0xFFFFFFFF);
 
-    set_page_flags(call_inst_location + 1, old_pte_val);
+    set_page_flags(jmp_inst_location + 1, old_pte_val);
 
     return true;
 }
--- winexts/hooks.h
+++ winexts/hooks.h
@@ -2,7 +2,7 @@
 #define HOOKS_H
 
 bool hook_rel_call(uintptr_t call_inst_location, uintptr_t new_func);
-bool hook_rel_jmp(uintptr_t call_inst_location, uintptr_t new_func);
+bool hook_rel_jmp(uintptr_t jmp_inst_location, uintptr_t new_func);
 
 int init_hooks(void);
 void clean_hooks(void);
