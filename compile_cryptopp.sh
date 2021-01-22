894d2ec925d7956167fac87dd97316f86fa7da53
XutaxKamay <xutaxkamay@gmail.com>
2021-01-22 16:24:40 +0100

    Updated crosscompile

--- compile_cryptopp.sh
+++ compile_cryptopp.sh
@@ -6,8 +6,8 @@ make clean
 CXX='g++ -m32' make -j8
 sudo cp libcryptopp.a /usr/local/lib/libcryptopp32.a
 make clean
-CXX='x86_64-w64-mingw32-g++' make -j8
+AR='x86_64-w64-mingw32-ar' RANLIB='x86_64-w64-mingw32-ranlib' CXX='x86_64-w64-mingw32-g++' CC='x86_64-w64-mingw32-gcc' make -j8
 sudo cp libcryptopp.a /usr/local/lib/libcryptoppwin.a
 make clean
-CXX='i686-w64-mingw32-g++' make -j8
+AR='i686-w64-mingw32-ar' RANLIB='i686-w64-mingw32-ranlib' CXX='i686-w64-mingw32-g++' CC='i686-w64-mingw32-gcc' make -j8
 sudo cp libcryptopp.a /usr/local/lib/libcryptopp32win.a
