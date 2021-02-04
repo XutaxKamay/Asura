2f909400fe418cb6e48a0e82f043081257c346f8
XutaxKamay <xutaxkamay@gmail.com>
2021-02-04 17:59:59 +0100

    Fixed header

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 CXX='g++' make install &
 CXX='g++ -m32' make install &
 AR='x86_64-w64-mingw32-ar' RANLIB='x86_64-w64-mingw32-ranlib' CXX='x86_64-w64-mingw32-g++' CC='x86_64-w64-mingw32-gcc' make install &
-AR='i686-w64-mingw32-ar' RANLIB='i686-w64-mingw32-ranlib' CXX='i686-w64-mingw32-g++' CC='i686-w64-mingw32-gcc' make install
+AR='i686-w64-mingw32-ar' RANLIB='i686-w64-mingw32-ranlib' CXX='i686-w64-mingw32-g++' CC='i686-w64-mingw32-gcc' make install &
 wait
