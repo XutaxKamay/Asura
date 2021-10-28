bdeba12ad4f6bd4694ffd8e0e5d35de47f4fa16b
XutaxKamay <admin@xutaxkamay.com>
2021-10-29 01:24:25 +0200

    Updated syscall number

--- src/custom_linux_syscalls.h
+++ src/custom_linux_syscalls.h
@@ -4,7 +4,7 @@
 namespace XLib
 {
 #ifndef _WIN32
-    constexpr auto __NR_custom_start   = 448;
+    constexpr auto __NR_custom_start   = 449;
     constexpr auto __NR_rmmap          = __NR_custom_start;
     constexpr auto __NR_rmprotect      = __NR_custom_start + 1;
     constexpr auto __NR_pkey_rmprotect = __NR_custom_start + 2;
