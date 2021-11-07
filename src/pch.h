949b69143d3169a3ad628a10e250c674cf7547bf
XutaxKamay <admin@xutaxkamay.com>
2021-11-07 15:56:44 +0100

    Fix windows build (3)

--- src/pch.h
+++ src/pch.h
@@ -80,15 +80,15 @@
 #include <unistd.h>
 
 /* CryptoPP */
-#include <vendor/cryptopp/aes.h>
-#include <vendor/cryptopp/modes.h>
-#include <vendor/cryptopp/osrng.h>
-#include <vendor/cryptopp/randpool.h>
-#include <vendor/cryptopp/rdrand.h>
-#include <vendor/cryptopp/rng.h>
-#include <vendor/cryptopp/rsa.h>
-#include <vendor/cryptopp/sha.h>
-#include <vendor/cryptopp/zlib.h>
+#include <../vendor/cryptopp/aes.h>
+#include <../vendor/cryptopp/modes.h>
+#include <../vendor/cryptopp/osrng.h>
+#include <../vendor/cryptopp/randpool.h>
+#include <../vendor/cryptopp/rdrand.h>
+#include <../vendor/cryptopp/rng.h>
+#include <../vendor/cryptopp/rsa.h>
+#include <../vendor/cryptopp/sha.h>
+#include <../vendor/cryptopp/zlib.h>
 
 /* SIMD */
 #include <immintrin.h>
